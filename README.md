473cf2dee9824bb9dc74ac197c61e289a7a0ecfa
Badal <badalWorks1@gmail.com>
2023-11-16 23:50:40 +0530

    mysql database integration

--- README.md
+++ README.md
@@ -39,6 +39,15 @@ mydockerproject/
    - Exposed Port: 80 
    - This services uses Nginx to serve a static HTML page.
 
+4. MySQL Database (`mysql-db` service)
+    - This service runs a MySQL Database 
+    - Port: 3306 
+    - Environment Variables:
+        - `MYSQL_ROOT_PASSWORD`: Root password for MySQL
+        - `MYSQL_DATABASE`: Name of the database
+        - `MYSQL_USER`: User for the database
+        - `MYSQL_PASSWORD`: Password for the database user
+
 ## How to Run 
 1. Make sure you have Docker and Docker Compose installed on your machine.
 1. Open a terminal and navigate to the project root directory.
@@ -47,3 +56,16 @@ mydockerproject/
    - Python Flask Backend: `http://localhost:5000` 
    - Go Service: `http://localhost:8080`
    - Frotend: `http://localhost` 
+
+
+## TODO 
+- [] Database Integration 
+- [] Logging and Monitoring 
+- [] Environment Configuration
+- [] Automated Testing 
+- [] CI/CD 
+- [] Security Scanning 
+- [] Networking and Service Communication 
+- [] Container Registry 
+- [] Documentation 
+
--- backend/go-service/main.go
+++ backend/go-service/main.go
@@ -1,16 +1,69 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello from Go service!")
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	router := mux.NewRouter()
+
+	db, err := sql.Open("mysql", "example_user:example_password@tcp(mysql-db:3306)/example_database")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	router.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		users, err := getAllUsers(db)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error retrieving users: %v", err), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(users)
+	}).Methods("GET")
+
+	log.Println("Server is running on :8080")
+	http.ListenAndServe(":8080", router)
+}
+
+func getAllUsers(db *sql.DB) ([]User, error) {
+	rows, err := db.Query("SELECT id, username, email FROM users")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []User
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.ID, &user.Username, &user.Email)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	return users, nil
 }
 
